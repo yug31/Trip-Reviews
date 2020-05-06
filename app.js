7f49692c8e5c03d7f30e9177ed1a738a09a17aa8
ullsee <61946112+ullsee@users.noreply.github.com>
2020-05-06 23:20:28 +0530

    server change

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
-  console.log("Server Has Started!");
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Server started");
 });
